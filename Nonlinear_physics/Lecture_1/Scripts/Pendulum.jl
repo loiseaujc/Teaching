85472e69c46b365734373bb4dcb4cb169b0c228c
JC <loiseau.jc@gmail.com>
2020-01-07 19:05:55 +0100

    Update lecture 1 slide with new figures.

--- Nonlinear_physics/Lecture_1/Scripts/Pendulum.jl
+++ Nonlinear_physics/Lecture_1/Scripts/Pendulum.jl
@@ -5,10 +5,10 @@ using DifferentialEquations
 using LinearAlgebra
 
 # -->
-using Plots; pyplot()
+using Plots; gr()
 using LaTeXStrings
 
-function pendulum_dynamics!(du, u, p, t)
+function dynamics!(du, u, p, t)
 
     # --> Unpack parameters.
     k, ω₀ = p
@@ -20,35 +20,124 @@ function pendulum_dynamics!(du, u, p, t)
     return du
 end
 
+function polar_to_cartesian(t, sol)
+
+    x = sin.(sol(t)[1, :])
+    y = -cos.(sol(t)[1, :])
+
+    return [x y]
+end
+
+function plot_pendulum(i, x)
+
+    p = plot(
+        [0, x[i, 1]], [0, x[i, 2]],
+        size=(512, 512),
+        xlim=(-1.1, 1.1),
+        ylim=(-1.5, 1.5),
+        color=1,
+        label="",
+        # markersize=15,
+        # markershape=:circle,
+        # label="",
+        # framestyle=:none,
+        # aspect_ratio=:equal
+    )
+
+    p = plot!(
+        [x[i, 1], x[i, 1]], [x[i, 2], x[i, 2]],
+        size=(512, 512),
+        xlim=(-1.175, 1.175),
+        ylim=(-1.5, 1.5),
+        color=1,
+        markersize=15,
+        markershape=:circle,
+        label="",
+        framestyle=:none,
+        # aspect_ratio=:equal
+    )
+
+    # -->
+    if i > 9*2
+        p = plot!([x[i-3*2:i, 1]], [x[i-3*2:i, 2]],alpha = 0.15,linewidth = 2, color = :red,label ="");
+        p = plot!([x[i-5*2:i-3*2, 1]], [x[i-5*2:i-3*2, 2]],alpha = 0.08,linewidth = 2, color = :red,label ="");
+        p = plot!([x[i-7*2:i-5*2, 1]], [x[i-7*2:i-5*2, 2]],alpha = 0.04,linewidth = 2, color = :red, label ="");
+        p = plot!([x[i-9*2:i-7*2, 1]], [x[i-9*2:i-7*2, 2]],alpha = 0.01,linewidth = 2, color = :red, label="");
+    end
+
+    return
+end
+
 function main()
 
     # -->
     tspan = (0.0, 25.0)
+    Δt = 0.025
+    t = collect(tspan[1]:Δt:tspan[2])
 
     # --> Parameters.
-    k, ω₀ = 0.25, 1.0
+    k, ω₀ = 0.01, 1.0
     params = (k, ω₀)
 
     # -->
-    p = plot()
+    x₀ = [3π/4, 0.0]
 
-    for i ∈ -3:3
-        for j ∈ -3:3
-            x₀ = [i * π/4, j * π/4]
+    # --> Define the problem.
+    prob = ODEProblem(dynamics!, x₀, tspan, p=params)
 
-            # --> Define the problem.
-            pendulum_prob = ODEProblem(pendulum_dynamics!, x₀, tspan, p=params)
+    # --> Simulated the problem.
+    sol = solve(prob, Tsit5())
 
-            # --> Simulated the problem.
-            pendulum_sol = solve(pendulum_prob, Tsit5())
+    # -->
+    x = polar_to_cartesian(t, sol)
 
-            p = plot!(
-                pendulum_sol, vars=(1, 2),
-                color=:chartreuse,
-                legend=false
-            )
-        end
-    end
+    # -->
+    # anim = @animate for i = 1:length(t)
+    #     p = plot_pendulum(i, x);
+    #
+    #     if i == 250
+    #         savefig("pendulum_image.png")
+    #     end
+    # end
+    #
+    # # -->
+    # gif(anim, "single_pendulum.gif", fps=60)
+    # mp4(anim, "single_pendulum.mp4", fps=60)
 
-    return p
+    p = plot(
+        [-0.05, -0.05], [0, -1],
+        color=1,
+        label="",
+    )
+
+    p = plot!(
+        [-0.05, -0.05], [-1, -1],
+        color=1,
+        markersize=15,
+        markershape=:circle,
+        label="",
+    )
+
+    p = plot!(
+        [0.05, 0.05], [0, 1],
+        color=2,
+        label="",
+    )
+
+    p = plot!(
+        [0.05, 0.05], [1, 1],
+        size=(128, 512),
+        xlim=(-0.15, 0.15),
+        ylim=(-1.175, 1.175),
+        color=2,
+        markersize=15,
+        markershape=:circle,
+        label="",
+        framestyle=:none,
+        aspect_ratio=:equal
+    )
+
+    savefig("pendulum_equilibria.png")
+
+    return
 end
